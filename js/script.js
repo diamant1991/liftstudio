cc67ef72249ba24cd7e6a4a7f251123f6d9dc0f5
diamant1991 <diamant1988@mail.ru>
2015-07-28 18:04:06 +0300

    add other files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('input,textarea').focus(function(){
+  $(this).data('placeholder',$(this).attr('placeholder'))
+  $(this).attr('placeholder','');
+});
+$('input,textarea').blur(function(){
+  $(this).attr('placeholder',$(this).data('placeholder'));
+});
+
+
+$(document).ready(function() {
+	 $('a[href*=#]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top - 96
+      }, 1300);
+      e.preventDefault();
+   });
+   return false;
+});
