f971b6a78883736ab1025226a91fcfc8b4d62df9
diamant1991 <diamant1988@mail.ru>
2015-07-30 18:07:49 +0300

    add other files

--- js/script.js
+++ js/script.js
@@ -6,8 +6,22 @@ $('input,textarea').blur(function(){
   $(this).attr('placeholder',$(this).data('placeholder'));
 });
 
+$(function(){
+    $(window).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top ==0) $(".menu-fixed").slideDown(250);
+        else $(".menu-fixed").slideUp(350);
+    });
+});
 
 $(document).ready(function() {
+    $('.navbar-fixed-top').hover(
+      function () {
+        if(($('.menu-fixed').css('display')=='none')){
+          $('.menu-fixed').slideDown(250);
+        }
+  });
+    
 	 $('a[href*=#]').bind("click", function(e){
       var anchor = $(this);
       $('html, body').stop().animate({
@@ -16,4 +30,6 @@ $(document).ready(function() {
       e.preventDefault();
    });
    return false;
+
+ 
 });
